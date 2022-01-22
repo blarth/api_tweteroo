5c21cfb8ddd3a3e80a9314fff7935f26a2e913ae
blarth <jminocente@gmail.com>
2022-01-22 13:43:33 -0300

    Managing data with global var and post sign-up working

--- api.js
+++ api.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import cors from "cors"
 
-const app = express(cors()); 
+const app = express(); 
 app.use(express.json());
 
+app.use(cors())
 
-const pessoas = [];
+const dbUser = []
+const dbTweet = []
 
-app.post('/', (req, res) => {
+
+app.post('/sign-up', (req, res) => {
+  const user = req.body
+  dbUser.push(user)
+  res.send("OK")
   
 });
 
