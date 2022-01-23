6c0acc91c3a4e906fab035dca630193bf34e2e36
blarth <jminocente@gmail.com>
2022-01-23 17:19:14 -0300

    feat: user is now in the header, fix: error messages

--- api.js
+++ api.js
@@ -1,82 +1,63 @@
-import express from 'express';
-import cors from "cors"
-import { body, validationResult, check } from 'express-validator';
+import express from "express";
+import cors from "cors";
+import { body, validationResult, check } from "express-validator";
 
-const app = express(); 
+const app = express();
 app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
-const dbUser = []
-const dbTweet = []
+const dbUser = [];
+const dbTweet = [];
 
+app.post(
+  "/sign-up",
+  check("username").isLength({ min: 1 }),
+  check("avatar").isLength({ min: 1 }),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send("Todos os Campos sao obrigatórios");
+    }
 
+    const user = req.body;
 
-app.post('/sign-up',
-check('username')
-    .isLength({ min: 1 })
-    .withMessage('Todos os Campos sao obrigatórios'),
-check('avatar')
-    .isLength({ min: 5 })
-    .withMessage('Todos os Campos sao obrigatórios'),
+    dbUser.push(user);
+    res.status(201).send("OK");
+  }
+);
 
- (req, res) => {
+app.post("/tweets", check("tweet").isLength({ min: 1 }), (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(400).send("Todos os Campos sao obrigatórios");
   }
 
+  const tweetVal = req.header("user");
+  const tweet = req.body;
 
-  const user = req.body
-  
-  
-  dbUser.push(user)
-  res.status(201).send("OK")
-  
-  
-});
-
-app.post('/tweets',
-check('username')
-    .isLength({ min: 1 })
-    .withMessage('Todos os Campos sao obrigatórios'),
-check('tweet')
-    .isLength({ min: 5 })
-    .withMessage('Todos os Campos sao obrigatórios'),
- (req, res) => {
-
-
-    const tweet = req.body
-    const fetchUser = dbUser.find(infoUser => tweet.username === infoUser.username)
-    dbTweet.push({...fetchUser, tweet : tweet.tweet})
-    
+  const fetchUser = dbUser.find((infoUser) => tweetVal === infoUser.username);
+  dbTweet.push({ ...fetchUser, tweet: tweet.tweet });
 
   res.status(201).send("OK");
 });
 
-app.get("/tweets" , (req, res) => {
-    const lastTweets = dbTweet.slice(-10)
-    res.send(lastTweets)
+app.get("/tweets", (req, res) => {
+  const page = parseInt(req.query.page);
 
+  const lastTweets = dbTweet.slice(-10);
+  res.send(lastTweets);
+});
+app.get("/tweets/:username", (req, res) => {
+  const tweetUser = req.params.username;
 
-})
-app.get("/tweets/:username" , (req, res) => {
+  const lastTweetsUser = dbTweet.filter(
+    (tweets) => tweets.username === tweetUser
+  );
 
-  const tweetUser = req.params.username
-
-  console.log(tweetUser)
-  
-
-  const lastTweetsUser = dbTweet.filter(tweets => tweets.username === tweetUser)
-  console.log(lastTweetsUser)
-  res.send(lastTweetsUser)
-
-
-})
-
-app.listen(5000, () => {
-
-    console.log("Server is running")
+  res.send(lastTweetsUser);
 });
 
-
+app.listen(5000, () => {
+  console.log("Server is running");
+});
