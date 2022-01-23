5376832e55a242cca0859e0edd1879972a52c3ad
blarth <jminocente@gmail.com>
2022-01-23 15:34:38 -0300

    feat: validations done, status code for post done

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from "cors"
+import { body, validationResult, check } from 'express-validator';
 
 const app = express(); 
 app.use(express.json());
@@ -10,21 +11,47 @@ const dbUser = []
 const dbTweet = []
 
 
-app.post('/sign-up', (req, res) => {
+
+app.post('/sign-up',
+check('username')
+    .isLength({ min: 1 })
+    .withMessage('Todos os Campos sao obrigatórios'),
+check('avatar')
+    .isLength({ min: 5 })
+    .withMessage('Todos os Campos sao obrigatórios'),
+
+ (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+
   const user = req.body
+  
+  
   dbUser.push(user)
-  res.send("OK")
+  res.status(201).send("OK")
   
   
 });
 
-app.post('/tweets', (req, res) => {
+app.post('/tweets',
+check('username')
+    .isLength({ min: 1 })
+    .withMessage('Todos os Campos sao obrigatórios'),
+check('tweet')
+    .isLength({ min: 5 })
+    .withMessage('Todos os Campos sao obrigatórios'),
+ (req, res) => {
+
+
     const tweet = req.body
     const fetchUser = dbUser.find(infoUser => tweet.username === infoUser.username)
     dbTweet.push({...fetchUser, tweet : tweet.tweet})
     
 
-  res.send("OK");
+  res.status(201).send("OK");
 });
 
 app.get("/tweets" , (req, res) => {
